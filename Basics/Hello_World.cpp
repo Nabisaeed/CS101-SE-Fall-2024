83d2e8aeb5393006dbd7cca07555de0b228ad758
Said Nabi <117791475+Nabisaeed@users.noreply.github.com>
2024-09-24 07:26:28 +0500

    Update Hello_World.cpp

--- Basics/Hello_World.cpp
+++ Basics/Hello_World.cpp
@@ -3,8 +3,13 @@
 
 // Define the main function, the program's entry point
 int main() {
+    
+    int cout=30;  // Declare a variable and assign 30
+    
     // Output "Hello World" to the console followed by a newline
     std::cout << "Hello World" << std::endl;
+
+    std::cout<<cout<<std::endl; // Output the value of variable cout
     
     // Return 0 to indicate successful program execution
     return 0;
